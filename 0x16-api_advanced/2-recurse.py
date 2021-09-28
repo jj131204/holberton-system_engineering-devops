80e79ed32ec15005671a17341ce501a76f2a8d85
root <root@LAPTOP-4MQ15EMS.localdomain>
2021-09-28 10:54:43 -0500

    2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" ... """
+
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ ... """
+    if not after:
+        url = 'https://www.reddit.com/r/{}/hot.json?'.format(subreddit)
+    else:
+        url = 'https://www.reddit.com/r/{}/hot.json?after={}'\
+            .format(subreddit, after)
+    head = {"User-Agent": "jj131204"}
+    test = requests.get(url, headers=head).json()
+
+    if test.get('error') == 404:
+        print('None')
+
+    else:
+        data = test.get('data').get('children')
+        for obj in data:
+            hot_list.append(obj.get('data').get('title'))
+        after = test.get('data').get('after')
+        if not after:
+            return hot_list
+        return recurse(subreddit, hot_list, after)
